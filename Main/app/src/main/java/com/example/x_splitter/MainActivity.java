37f807342bb58a0a60dc07901d89e4a27bd018b3
Namkong Kirat <namkongkirat@gmail.com>
2019-05-03 16:53:44 +0545

    Main project

---
+++ Main/app/src/main/java/com/example/x_splitter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.x_splitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_page);
+        //etContentView(R.layout.login_page);
+        //setContentView(R.layout.activity_main);
+    }
+}
