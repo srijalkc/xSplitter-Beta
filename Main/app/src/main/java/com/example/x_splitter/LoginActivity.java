ebeff9f637257f8fb9c21447324ed3e239a5b6d1
srijalkc <srijalkc2017@gmail.com>
2019-05-04 12:08:18 +0545

    Applied UI Actions
    Database has not been set up. Only UI.

--- Main/app/src/main/java/com/example/x_splitter/LoginActivity.java
+++ Main/app/src/main/java/com/example/x_splitter/LoginActivity.java
@@ -3,13 +3,10 @@ package com.example.x_splitter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up_page);
-        //etContentView(R.layout.login_page);
-        //setContentView(R.layout.activity_main);
     }
 }
