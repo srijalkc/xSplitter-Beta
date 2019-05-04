ec8bd44fc001776b564db3c1450b43b94390bb17
srijalkc <srijalkc2017@gmail.com>
2019-05-04 15:01:13 +0545

    SignUp with Database
    Included jdbc, but not working

--- Main/app/src/main/java/com/example/x_splitter/SignUpRegister.java
+++ Main/app/src/main/java/com/example/x_splitter/SignUpRegister.java
@@ -1,6 +1,37 @@
 package com.example.x_splitter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SignUpRegister {
+        Connection con = null;
+        public void connect(){
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost/users_db", "root", "");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("connect() method Executed");
+        }
+        public void addUsers(SignUpRegisterValues rv){
+            String query = "insert into users_info values (?,?,?,?)";
+            PreparedStatement pst;
+            try {
+                pst = con.prepareStatement(query);
+                pst.setString(1, rv.email);
+                pst.setString(2, rv.username);
+                pst.setString(3, rv.password);
+                pst.setString(4, rv.confirmPasswsord);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("addUsers() method Executed");
+        }
 }
+
+
 
 
--- Main/app/src/main/java/com/example/x_splitter/SignupActivity.java
+++ Main/app/src/main/java/com/example/x_splitter/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.x_splitter;
 
 import android.app.ProgressDialog;
+import android.icu.text.UnicodeSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -9,10 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.regex.Pattern;
 
-public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editTextEmail;
     private EditText editTextUsername;
     private EditText editTextPassword;
@@ -22,66 +21,26 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     private ProgressDialog progressDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        editTextEmail = (EditText)findViewById(R.id.edittext_email);
-        editTextUsername = (EditText)findViewById(R.id.edittext_username);
-        editTextPassword = (EditText)findViewById(R.id.edittext_password);
-        editTextConfirmPassword = (EditText)findViewById(R.id.edittext_confirm_password);
-        buttonSignUp = (Button)findViewById(R.id.button_signup);
-        buttonSignUpGmail = (Button)findViewById(R.id.button_signup_gmail);
+        editTextEmail = (EditText) findViewById(R.id.edittext_email);
+        editTextUsername = (EditText) findViewById(R.id.edittext_username);
+        editTextPassword = (EditText) findViewById(R.id.edittext_password);
+        editTextConfirmPassword = (EditText) findViewById(R.id.edittext_confirm_password);
+        buttonSignUp = (Button) findViewById(R.id.button_signup);
+        buttonSignUpGmail = (Button) findViewById(R.id.button_signup_gmail);
 
         progressDialog = new ProgressDialog(this);
 
         buttonSignUp.setOnClickListener(this);
+
     }
 
-    void registerUser() {
-        String Email = editTextEmail.getText().toString().trim();
-        String Username = editTextUsername.getText().toString().trim();
-        String Password = editTextPassword.getText().toString().trim();
-        String ConfirmPassword = editTextConfirmPassword.getText().toString().trim();
-
-        if(Email.isEmpty()){
-            Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show();
-            //editTextEmail.requestFocus();
-            return;
-            }
-        if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
-            Toast.makeText(this, "Email pattern is wrong", Toast.LENGTH_SHORT).show();
-            //editTextEmail.requestFocus();
-            return;
-        }
-        if(Username.isEmpty()){
-            Toast.makeText(this, "Username is required", Toast.LENGTH_SHORT).show();
-            //editTextUsername.requestFocus();
-            return;
-        }
-        if(Password.isEmpty()){
-            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
-            //editTextPassword.requestFocus();
-            return;
-        }
-        if(!Password.equals(ConfirmPassword)){
-            Toast.makeText(this, "Password mismatch", Toast.LENGTH_SHORT).show();
-            //editTextConfirmPassword.requestFocus();
-            return;
-        }
-        if(Password.length()<4){
-            Toast.makeText(this, "Minimum password length is 4", Toast.LENGTH_SHORT).show();
-            //editTextConfirmPassword.requestFocus();
-            return;
-        }
-        progressDialog.setMessage("Registering User");
-        progressDialog.show();
-        SignUpRegister register = new SignUpRegister();
-        SignUpRegisterValues registerValues = new SignUpRegisterValues();
-    }
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.button_signup:
                 registerUser();
                 break;
@@ -89,4 +48,57 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         }
 
     }
+
+    public void registerUser() {
+        String Email = editTextEmail.getText().toString().trim();
+        String Username = editTextUsername.getText().toString().trim();
+        String Password = editTextPassword.getText().toString().trim();
+        String ConfirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        if (Email.isEmpty()) {
+            Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show();
+            //editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+            Toast.makeText(this, "Email pattern is wrong", Toast.LENGTH_SHORT).show();
+            //editTextEmail.requestFocus();
+            return;
+        }
+        if (Username.isEmpty()) {
+            Toast.makeText(this, "Username is required", Toast.LENGTH_SHORT).show();
+            //editTextUsername.requestFocus();
+            return;
+        }
+        if (Password.isEmpty()) {
+            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            //editTextPassword.requestFocus();
+            return;
+        }
+        if (!Password.equals(ConfirmPassword)) {
+            Toast.makeText(this, "Password mismatch", Toast.LENGTH_SHORT).show();
+            //editTextConfirmPassword.requestFocus();
+            return;
+        }
+        if (Password.length() < 4) {
+            Toast.makeText(this, "Minimum password length is 4", Toast.LENGTH_SHORT).show();
+            //editTextConfirmPassword.requestFocus();
+            return;
+        }
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+        SignUpRegisterValues registerValues = new SignUpRegisterValues();
+        registerValues.username= Username;
+        registerValues.email= Email;
+        registerValues.password= Password;
+        registerValues.confirmPasswsord= ConfirmPassword;
+        SignUpRegister register = new SignUpRegister();
+        register.connect();
+        register.addUsers(registerValues);
+        progressDialog.setMessage("Registered");
+        progressDialog.show();
+    }
 }
+
+
+
