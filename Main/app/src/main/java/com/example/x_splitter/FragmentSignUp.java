e5393e8ea23ac4d3440e0482d6be0bc798f6c70b
NehaAdhikari <nehaadhikari81@gmail.com>
2019-05-06 17:16:59 +0545

    button activity to signup with email

--- Main/app/src/main/java/com/example/x_splitter/FragmentSignUp.java
+++ Main/app/src/main/java/com/example/x_splitter/FragmentSignUp.java
@@ -1,5 +1,6 @@
 package com.example.x_splitter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FragmentSignUp extends Fragment {
     View view;
@@ -18,6 +20,14 @@ public class FragmentSignUp extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.get_started_signup_fragment,container,false);
+        Button btnSignUpEmail = view.findViewById(R.id.btn_get_started_signup_email);
+        btnSignUpEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentSignUp.this.getActivity(),Signup.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
