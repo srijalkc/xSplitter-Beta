c7bbfe1974ee36dc958e905a54b720f06956427e
NehaAdhikari <nehaadhikari81@gmail.com>
2019-05-06 14:08:50 +0545

    Update IntroActivity.java

--- Main/app/src/main/java/com/example/x_splitter/IntroActivity.java
+++ Main/app/src/main/java/com/example/x_splitter/IntroActivity.java
@@ -43,20 +43,20 @@ public class IntroActivity extends AppCompatActivity {
 
         //fill list screen
 
-        final List<ScreenItem> mList = new ArrayList<>();
-        mList.add(new ScreenItem("Group",
+        final List<IntroScreenItem> mList = new ArrayList<>();
+        mList.add(new IntroScreenItem("Group",
                 "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. ",
                 R.drawable.undraw_group_selfie_ijc6));
-        mList.add(new ScreenItem("Events",
+        mList.add(new IntroScreenItem("Events",
                 "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. ",
                 R.drawable.undraw_having_fun_iais));
-        mList.add(new ScreenItem("Split",
+        mList.add(new IntroScreenItem("Split",
                 " Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. ",
                 R.drawable.undraw_make_it_rain_iwk4));
-        mList.add(new ScreenItem("Report",
+        mList.add(new IntroScreenItem("Report",
                 "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. ",
                 R.drawable.undraw_data_report_bi6l));
-        mList.add(new ScreenItem("Save Time",
+        mList.add(new IntroScreenItem("Save Time",
                 "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. ",
                 R.drawable.undraw_time_management_30iu));
 
