6d69ce931d3f21ffe06e3aa57b2ff3767a0eb3f7
Namkong Kirat <namkongkirat@gmail.com>
2019-05-11 10:27:24 +0545

    button images added

--- Main/app/src/main/java/com/example/x_splitter/FragmentSignUp.java
+++ Main/app/src/main/java/com/example/x_splitter/FragmentSignUp.java
@@ -24,7 +24,7 @@ public class FragmentSignUp extends Fragment {
         btnSignUpEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FragmentSignUp.this.getActivity(),Signup.class);
+                Intent intent = new Intent(FragmentSignUp.this.getActivity(),SignupActivity.class);
                 startActivity(intent);
             }
         });
