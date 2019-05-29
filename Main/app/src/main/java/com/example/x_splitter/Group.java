06427a670d16555c22d0fbaffc79c55d7f87709e
NehaAdhikari <nehaadhikari81@gmail.com>
2019-05-30 00:54:17 +0545

    Create Group.java

---
+++ Main/app/src/main/java/com/example/x_splitter/Group.java
@@ -0,0 +1,42 @@
+
+package com.example.x_splitter;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.design.widget.BottomNavigationView;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+
+public class Group extends AppCompatActivity {
+    FloatingActionButton fab_add;
+
+    private static final int Activity_num = 1; // for recognizing menu item number
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+
+        fab_add = findViewById(R.id.fab_add);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Group.this,FAB_Menu_page.class));
+            }
+        });
+
+        setBottomNavigationView();
+    }
+
+    private void setBottomNavigationView(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navbar);
+        BottomNavigationHelper.enableNavigation(Group.this,bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(Activity_num);
+        menuItem.setChecked(true);
+    }
+}
