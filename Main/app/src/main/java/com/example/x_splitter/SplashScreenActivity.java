bd54c27a43ba7955937b1e2e2dbc8ddf65e9e2e0
NehaAdhikari <nehaadhikari81@gmail.com>
2019-05-30 00:49:45 +0545

    nth

--- Main/app/src/main/java/com/example/x_splitter/SplashScreenActivity.java
+++ Main/app/src/main/java/com/example/x_splitter/SplashScreenActivity.java
@@ -14,7 +14,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, IntroActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, Home.class);
                 startActivity(i);
                 finish();
             }
